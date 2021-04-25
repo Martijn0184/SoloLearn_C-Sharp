635819a795819b8edc1fcafce9f03b6e39ad9330
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-04-25 15:48:51 +0200

    Add files via upload

---
+++ 036_Switch_statement/036_Switch_statement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _036_Switch_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+            }
+        }
+    }
+}
---
+++ 038_the_default_II/038_the_default_II/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _038_the_default_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 16;
+            switch (x)
+            {
+                case 10:
+                    Console.WriteLine("Ten");
+                    break;
+                case 20:
+                    Console.WriteLine("Twenty");
+                    break;
+                default:
+                    Console.WriteLine("No Match");
+                    break;
+            }
+        }
+    }
+}
