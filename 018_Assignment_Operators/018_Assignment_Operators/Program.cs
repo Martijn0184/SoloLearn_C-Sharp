170e2cd8a3bcfd30b0ea391b9b13937174d3fe0c
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-04-25 12:39:10 +0200

    Add files via upload

---
+++ 018_Assignment_Operators/018_Assignment_Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _018_Assignment_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 40;
+            x += 2; //equivalent to x = x + 2
+            Console.WriteLine(x);
+            int y = 48;
+            y -= 6; //equivalent to y = y - 6
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ 019_Assignment Operators_II/019_Assignment Operators_II/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _019_Assignment_Operators_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 21;
+            x *= 2; //equivalent to x = x * 2;
+            Console.WriteLine(x);
+
+            int y = 84;
+            y /= 2; //equivalent to y = y / 2;
+            Console.WriteLine(y);
+
+            int z = 542;
+            z %= 100; //equivalent to z = z % 100;
+            Console.WriteLine(z);
+        }
+    }
+}
