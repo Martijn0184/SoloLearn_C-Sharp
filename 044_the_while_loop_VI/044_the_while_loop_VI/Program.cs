d5b5d4f1e027a9a4d9b0b075e44e02649cd140cb
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-04-25 16:25:27 +0200

    Add files via upload

---
+++ 044_the_while_loop_VI/044_the_while_loop_VI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _044_the_while_loop_VI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while(x++<5)
+            {
+                if (x % 2 == 0)
+                    x += 2;
+            }
+        }
+    }
+}
---
+++ 045_the_for_loop/045_the_for_loop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _045_the_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x=10; x < 15; x++)
+            {
+                Console.WriteLine("Value of x: {0}", x);
+            }
+        }
+    }
+}
