739527b06d3c71bf605d566aa68b00beb7f2e97a
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-04-25 16:16:29 +0200

    Add files via upload

---
+++ 042_the_While_loop_IV/042_the_While_loop_IV/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _042_the_While_loop_IV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while (num < 100)
+            {
+                Console.WriteLine(num);
+                num += 2;
+            }
+        }
+    }
+}
---
+++ 043_the_while_loop_V/043_the_while_loop_V/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _043_the_while_loop_V
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while(++num < 6)
+            Console.WriteLine(num);
+        }
+    }
+}
