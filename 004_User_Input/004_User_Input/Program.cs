a48d22b30090e948a7686b3dfd11618020138073
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-04-25 12:02:42 +0200

    Add files via upload

---
+++ 004_User_Input/004_User_Input/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _004_User_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yourName;
+            Console.WriteLine("What is your name?");
+            yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}", yourName);
+        }
+    }
+}
---
+++ 005_User_Input_II/005_User_Input_II/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _005_User_Input_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are {0} years old.", age);
+        }
+    }
+}
