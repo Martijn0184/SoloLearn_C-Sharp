67de3e2e5d713395efc467e3c1633755ca88fd61
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-04-25 12:05:20 +0200

    Add files via upload

---
+++ 012_Arithmetic_Operators_Division/012_Arithmetic_Operators_Division/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _012_Arithmetic_Operators_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10 / 4;
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ 013_Arithmetic_Operators_Division_II/013_Arithmetic_Operators_Division_II/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _013_Arithmetic_Operators_Division_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 16;
+            int y = 5;
+            Console.WriteLine(x/y);
+        }
+    }
+}
