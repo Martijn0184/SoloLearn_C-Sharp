9622b18a0f78fafcecb62e0f5f46a04d6178b522
Martijn den Breejen <60103411+Martijn0184@users.noreply.github.com>
2021-04-25 12:19:29 +0200

    Add files via upload

---
+++ 017_Operator_Precedence_III/017_Operator_Precedence_III/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _017_Operator_Precedence_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = (2 + 3) * 3;
+            Console.WriteLine(x);
+        }
+    }
+}
